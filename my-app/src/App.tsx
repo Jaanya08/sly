054a9ed3752e8bed9f88fea094b203a4eec1fd24
Jaanya08 <jaanyabagdi@gmail.com>
2025-08-26 14:02:55 +0530

    Second

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,16 @@
-// src/App.tsx
-// import React from 'react';
+// // src/App.tsx
+// // import React from 'react';
+// import Map from './components/Map';
+
+// function App() {
+//   return (
+//     <div className="min-h-screen bg-black text-white p-4">
+//       {/* <h1 className="text-xl mb-4">Scotland Yard Map</h1> */}
+//       <Map />
+//     </div>
+//   );
+// }
+// export default App;
 import Map from './components/Map';
 
 function App() {
--- my-app/src/components/Map.tsx
+++ my-app/src/components/Map.tsx
@@ -4,7 +4,7 @@ import { nodes } from '../nodes';
 const Map: React.FC = () => {
   return (
     <div className="relative w-full max-w-5xl mx-auto">
-      <img src="../public/map.jpg" alt="Scotland Yard Map" className="w-full h-auto" />
+      <img src="/map.jpg" alt="Scotland Yard Map" className="w-full h-auto" />
 
       {nodes.map((node) => (
         <div
@@ -26,3 +26,36 @@ const Map: React.FC = () => {
 };
 
 export default Map;
+// import React from "react";
+// import { nodes } from "../nodes";
+
+// const Map: React.FC = () => {
+//   return (
+//     <div className="relative w-full max-w-5xl mx-auto">
+//       {/* ✅ Correct way to load from public/ */}
+//       <img
+//         src="/map.jpg"
+//         alt="Scotland Yard Map"
+//         className="w-full h-auto"
+//       />
+
+//       {/* ✅ Render nodes */}
+//       {nodes.map((node) => (
+//         <div
+//           key={node.id}
+//           className="absolute w-5 h-5 bg-red-500 rounded-full text-white text-[10px] flex items-center justify-center shadow-lg"
+//           style={{
+//             left: `${node.x * 100}%`,  // works if x,y are between 0 and 1
+//             top: `${node.y * 100}%`,
+//             transform: "translate(-50%, -50%)",
+//           }}
+//           title={`Node ${node.id}`}
+//         >
+//           {node.id}
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default Map;
